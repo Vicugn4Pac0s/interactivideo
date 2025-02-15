b51ce07b755400098228c638afebe0033a803421
Vicugn4Pac0s <vicugnapacos.g@gmail.com>
2025-02-16 00:08:05 +0900

    PlayerおよびCanvasPlayerをエクスポートし、utilsオブジェクトを更新。interactivideoオブジェクトをエクスポートするように変更。

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Player } from './player'
 import { CanvasPlayer } from './canvas-player'
 import { normalize } from './helpers'
 
-const utils = {
+export const player = Player;
+export const canvasPlayer = CanvasPlayer;
+export const utils = {
   normalize
 }
 
-export default {
+export const interactivideo = {
   Player,
   CanvasPlayer,
   utils
